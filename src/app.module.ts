fe6b73d91bc5608d21ac3f57cec7612d21a2dd8e
Amir Dhahri <amirdhahri01@gmail.com>
2025-01-03 23:15:49 +0100

    An update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookingControllerController } from './booking-controller/booking-controller.controller';
+import { BookingController } from './booking/booking.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, BookingControllerController, BookingController],
   providers: [AppService],
 })
 export class AppModule {}
